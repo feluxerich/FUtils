362a910ac988ae8a4b2d6037275c0eff7a288cd3
feluxerich <felix.waidler@web.de>
2023-01-01 15:38:39 +0100

    added reset command

--- src/main/java/dev/fluxi/futils/FUtils.java
+++ src/main/java/dev/fluxi/futils/FUtils.java
@@ -1,21 +1,22 @@
 package dev.fluxi.futils;
 
-import dev.fluxi.futils.commands.ChallengeCommand;
-import dev.fluxi.futils.commands.HealCommand;
-import dev.fluxi.futils.commands.TimerCommand;
-import dev.fluxi.futils.commands.VanishCommand;
+import dev.fluxi.futils.commands.*;
 import dev.fluxi.futils.listeners.JoinListener;
 import dev.fluxi.futils.listeners.PlayerInteractEntityListener;
 import dev.fluxi.futils.utils.Timer;
 import dev.fluxi.futils.utils.ChallengeManager;
 import dev.fluxi.futils.utils.VanishManager;
+import org.apache.commons.io.FileUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Objects;
 
 public final class FUtils extends JavaPlugin {
@@ -23,6 +24,31 @@ public final class FUtils extends JavaPlugin {
     private VanishManager vanishManager;
     private ChallengeManager challengeManager;
     private Timer timer;
+    FileConfiguration config = getConfig();
+
+    @Override
+    public void onLoad() {
+        if (!config.getBoolean("reset")) {
+            return;
+        }
+        File world = new File(Bukkit.getWorldContainer(), "world");
+        File nether = new File(Bukkit.getWorldContainer(), "world_nether");
+        File end = new File(Bukkit.getWorldContainer(), "world_the_end");
+
+        try {
+            FileUtils.deleteDirectory(world);
+            FileUtils.deleteDirectory(nether);
+            FileUtils.deleteDirectory(end);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        world.mkdirs();
+        new File(world, "playerdata").mkdirs();
+
+        config.set("reset", false);
+        saveConfig();
+    }
 
     @Override
     public void onEnable() {
@@ -30,6 +56,10 @@ public final class FUtils extends JavaPlugin {
         vanishManager = new VanishManager(this);
         challengeManager = new ChallengeManager();
         timer = new Timer(false, 0);
+
+        config.addDefault("reset", false);
+        config.options().copyDefaults(true);
+        saveConfig();
 
         registerCommands();
         registerEvents();
@@ -44,6 +74,7 @@ public final class FUtils extends JavaPlugin {
         registerCommand("heal", new HealCommand(), new HealCommand());
         registerCommand("challenge", new ChallengeCommand(), new ChallengeCommand());
         registerCommand("timer", new TimerCommand(), new TimerCommand());
+        registerCommand("reset", new ResetCommand(), new ResetCommand());
     }
 
     private void registerCommand(String name, CommandExecutor commandExecutor, TabCompleter tabCompleter) {
---
+++ src/main/java/dev/fluxi/futils/commands/ResetCommand.java
@@ -0,0 +1,39 @@
+package dev.fluxi.futils.commands;
+
+import dev.fluxi.futils.FUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ResetCommand implements CommandExecutor, TabCompleter {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (args.length < 1) {
+            sender.sendMessage(ChatColor.RED + "Confirm with: /reset confirm");
+            return true;
+        }
+        if (!args[0].equals("confirm")) {
+            return false;
+        }
+
+        FUtils.getInstance().getConfig().set("reset", true);
+        FUtils.getInstance().saveConfig();
+
+        Bukkit.shutdown();
+
+        return true;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return Collections.singletonList("confirm");
+    }
+}
