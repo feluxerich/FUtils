8e6cb0b4068b2d7cd2a1a1a49b6548930a1bc082
feluxerich <felix.waidler@web.de>
2023-01-01 18:54:15 +0100

    improved resetcommand

--- src/main/java/dev/fluxi/futils/commands/ResetCommand.java
+++ src/main/java/dev/fluxi/futils/commands/ResetCommand.java
@@ -1,12 +1,15 @@
 package dev.fluxi.futils.commands;
 
 import dev.fluxi.futils.FUtils;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -23,12 +26,16 @@ public class ResetCommand implements CommandExecutor, TabCompleter {
         if (!args[0].equals("confirm")) {
             return false;
         }
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            player.kick(Component.text("Server resetting")
+                    .color(TextColor.fromHexString("#ef4444"))
+                    .toBuilder().build());
+        }
 
         FUtils.getInstance().getConfig().set("reset", true);
         FUtils.getInstance().saveConfig();
 
-        Bukkit.shutdown();
-
+        Bukkit.spigot().restart();
         return true;
     }
 
