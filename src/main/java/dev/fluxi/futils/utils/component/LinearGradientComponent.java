417f65baeedd2d4c9cbd726860f1d03705fc1888
Felix <68397506+feluxerich@users.noreply.github.com>
2023-01-06 13:46:18 +0100

    added gradient component

---
+++ src/main/java/dev/fluxi/futils/utils/component/LinearGradientComponent.java
@@ -0,0 +1,62 @@
+package dev.fluxi.futils.utils.component;
+
+import dev.fluxi.futils.utils.colors.LinearGradient;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.TextColor;
+
+public class LinearGradientComponent {
+    private String text;
+    private TextColor fromColor;
+    private TextColor toColor;
+    private Component gradientComponent;
+
+    public LinearGradientComponent(String text, TextColor fromColor, TextColor toColor) {
+        setText(text);
+        setFromColor(fromColor);
+        setToColor(toColor);
+
+        setGradientComponent(createGradientComponent());
+    }
+
+    private Component createGradientComponent() {
+        TextComponent.Builder component = Component.text();
+        LinearGradient gradient = new LinearGradient(getFromColor(), getToColor(), getText().length());
+        for (int i = 0; i < getText().length(); i++) {
+            component.append(Component.text(getText().charAt(i)).color(gradient.getColorByPosition(i)));
+        }
+        return component.build();
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public TextColor getFromColor() {
+        return fromColor;
+    }
+
+    public void setFromColor(TextColor fromColor) {
+        this.fromColor = fromColor;
+    }
+
+    public TextColor getToColor() {
+        return toColor;
+    }
+
+    public void setToColor(TextColor toColor) {
+        this.toColor = toColor;
+    }
+
+    public Component getGradientComponent() {
+        return gradientComponent;
+    }
+
+    public void setGradientComponent(Component gradientComponent) {
+        this.gradientComponent = gradientComponent;
+    }
+}
