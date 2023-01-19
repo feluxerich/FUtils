2181d71edfe4a3a2a548f0d712635df8839c3bcd
Felix <felix.waidler@web.de>
2023-01-19 13:58:46 +0100

    made dev message

--- src/main/java/dev/fluxi/futils/listeners/PlayerJoinListener.java
+++ src/main/java/dev/fluxi/futils/listeners/PlayerJoinListener.java
@@ -1,8 +1,8 @@
 package dev.fluxi.futils.listeners;
 
-import dev.fluxi.futils.components.LinearGradientComponent;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.Style;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -10,12 +10,11 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class PlayerJoinListener implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        if (!event.getPlayer().getName().equals("Rumsbums_")) {
+        if (!event.getPlayer().isOp()) {
             return;
         }
-        event.getPlayer().playerListName(
-                new LinearGradientComponent("[DEV]", TextColor.fromHexString("#5b45ff"), TextColor.fromHexString("#8b5cf6")).getGradientComponent().append(Component.text(" ")).append(
-                        new LinearGradientComponent(event.getPlayer().getName(), TextColor.fromHexString("#8b5cf6"), TextColor.fromHexString("#5b45ff")).getGradientComponent()
-                ));
+        event.getPlayer().sendMessage(
+                Component.text("This Plugin is still under hard development.", Style.style(NamedTextColor.LIGHT_PURPLE))
+        );
     }
 }
