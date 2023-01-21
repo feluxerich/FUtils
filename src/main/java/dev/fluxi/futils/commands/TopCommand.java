e397490c9a0976e89e1bd2e4e395c7654f2be440
Felix <felix.waidler@web.de>
2023-01-21 21:03:44 +0100

    added top command

---
+++ src/main/java/dev/fluxi/futils/commands/TopCommand.java
@@ -0,0 +1,35 @@
+package dev.fluxi.futils.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class HealCommand implements CommandExecutor, TabCompleter {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "You have to be a Player");
+            return true;
+        }
+        Player player = (Player) sender;
+
+        double x = player.getLocation().getX();
+        double z = player.getLocation().getZ();
+        int y = player.getWorld().getHighestBlockYAt((int) x, (int) z);
+
+        player.teleport(new Location(player.getWorld(), (int) x, y + 1, (int) z));
+        return true;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return null;
+    }
+}
