67093cfc157f9aef9ee2bedf66acef1cd4b026eb
Felix <felix.waidler@web.de>
2023-01-28 22:16:27 +0100

    fixed settings activation

--- src/main/java/dev/fluxi/futils/settings/utils/Setting.java
+++ src/main/java/dev/fluxi/futils/settings/utils/Setting.java
@@ -1,6 +1,5 @@
 package dev.fluxi.futils.settings.utils;
 
-import dev.fluxi.futils.FUtils;
 import dev.fluxi.futils.inventory.items.Item;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
@@ -12,12 +11,10 @@ import java.util.List;
 public class Setting extends Item implements Listener {
     public Setting(Material material, Component name) {
         super(material, name);
-        FUtils.getInstance().registerEvent(this);
     }
 
     public Setting(Material material, Component name, List<Component> description) {
         super(material, name, description);
-        FUtils.getInstance().registerEvent(this);
     }
 
     @Override
